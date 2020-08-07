f0d89d681a8fe8609f71507b3472c546573ea1bb
Amit998 <damitlucky998@gmail.com>
2020-08-07 22:15:38 +0530

    Only Head Is Moving

---
+++ lib/src/screens/home.dart
@@ -0,0 +1,77 @@
+import 'package:flutter/material.dart';
+import '../widgets/cat.dart';
+
+class Home extends StatefulWidget{
+
+  HomeState createState() => HomeState();
+
+
+}
+class HomeState extends State<Home> with TickerProviderStateMixin{
+  Animation<double> catAnimation;
+  AnimationController catController;
+
+  initState(){
+    super.initState();
+
+    catController=AnimationController(
+      duration: Duration(seconds: 2),
+      vsync: this,
+    );
+    catAnimation= Tween(begin: 0.0,end: 100.0).animate(
+      CurvedAnimation(
+        parent: catController,
+        curve: Curves.easeIn,
+      ),
+    );
+
+  }
+  onTap(){
+
+    if(catController.status == AnimationStatus.completed){
+      catController.reverse();
+    }else if(catController.status == AnimationStatus.dismissed){
+      catController.forward();
+      print("lol");
+    }
+  }
+
+  Widget build(context){
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Animation 3'),
+      ),
+      body: GestureDetector(
+        child: Center(
+          child: Stack(
+            children: <Widget>[
+              buildCatAnimation(),
+              buildBox(),
+            ],
+          ),
+        ),
+        onTap: onTap,
+      ),
+    );
+
+  }
+  Widget buildCatAnimation(){
+    return AnimatedBuilder(
+      animation: catAnimation,
+      builder: (context,child){
+        return Container(
+          child: child,
+          margin: EdgeInsets.only(top: catAnimation.value),
+        );
+      },
+      child: Cat(),
+    );
+  }
+  Widget buildBox(){
+    return Container(
+      height: 200.0,
+      width: 200.0,
+      color: Colors.brown,
+    );
+  }
+}
